3d78193b761ee4d6b4f9e30261bc777f999060a1
Felix Morgner <felix.morgner@hsr.ch>
2019-06-13 16:22:53 +0200

    enumerate: beautify output

--- src/enumerate.cpp
+++ src/enumerate.cpp
@@ -28,11 +28,12 @@ void map(std::optional<ScopedType> const & scoped, Callable callable)
 }
 
 void visit(udevxx::device const & device,
-           void (*callable)(udevxx::device const &, int, std::ostream &),
+           void (*callable)(udevxx::device const &, char const *, int, std::ostream &),
+           char const * prefix = "device",
            std::ostream & out = std::cout,
            int depth = 0)
 {
-  callable(device, depth, out);
+  callable(device, prefix, depth, out);
 }
 
 std::ostream & indent(std::ostream & out, int depth)
@@ -44,20 +45,20 @@ std::ostream & indent(std::ostream & out, int depth)
   return out;
 }
 
-void print(udevxx::device const & device, int depth, std::ostream & out = std::cout)
+void print(udevxx::device const & device, char const * prefix, int depth, std::ostream & out = std::cout)
 {
-  indent(out, depth) << "device: " << device.system_path() << '\n';
+  indent(out, depth) << prefix << ": " << device.system_path() << '\n';
   indent(out, depth + 1) << "subsystem: " << device.subsystem() << '\n';
   indent(out, depth + 1) << "sysname: " << device.system_name() << '\n';
   indent(out, depth + 1) << "tags: \n";
   map(device.tags(), [&](auto const & tag) { indent(out, depth + 2) << "- " << tag << '\n'; });
-  map(device.parent(), [&](auto const & parent) { visit(parent, print, out, depth + 1); });
+  map(device.parent(), [&](auto const & parent) { visit(parent, print, "parent", out, depth + 1); });
 }
 
 int main()
 {
   auto context = udevxx::context{};
-  auto devices = context.devices().match(in("leds"_sub));
+  auto devices = context.devices();
 
   for (auto const & device : devices)
   {

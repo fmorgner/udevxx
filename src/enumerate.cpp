5a9747c2b96e963364b1dc284266306586ff63c1
Felix Morgner <felix.morgner@hsr.ch>
2019-06-14 13:23:52 +0200

    enumerate: clean up code

--- src/enumerate.cpp
+++ src/enumerate.cpp
@@ -7,76 +7,84 @@
 using namespace udevxx::literals;
 using namespace udevxx::manipulators;
 
-template <typename ScopedType, typename Callable>
-void map(std::vector<ScopedType> const & scoped, Callable callable)
+inline namespace support
 {
-  if (!scoped.empty())
+  template <typename ScopedType, typename Callable>
+  void map(std::vector<ScopedType> const & scoped, Callable callable)
+  {
+    if (!scoped.empty())
+    {
+      for (auto const & value : scoped)
+      {
+        callable(value);
+      }
+    }
+  }
+
+  template <typename KeyType, typename ValueType, typename Callable>
+  void map(std::map<KeyType, ValueType> const & scoped, Callable callable)
   {
     for (auto const & value : scoped)
     {
       callable(value);
     }
   }
-}
 
-template <typename KeyType, typename ValueType, typename Callable>
-void map(std::map<KeyType, ValueType> const & scoped, Callable callable)
-{
-  for (auto const & value : scoped)
+  template <typename ScopedType, typename Callable>
+  void map(std::optional<ScopedType> const & scoped, Callable callable)
   {
-    callable(value);
+    if (scoped)
+    {
+      callable(scoped.value());
+    }
   }
-}
 
-template <typename ScopedType, typename Callable>
-void map(std::optional<ScopedType> const & scoped, Callable callable)
+}  // namespace support
+
+inline namespace traverse
 {
-  if (scoped)
+  void visit(udevxx::device const & device,
+             void (*callable)(udevxx::device const &, char const *, int, std::ostream &),
+             char const * prefix = "device",
+             std::ostream & out = std::cout,
+             int depth = 0)
   {
-    callable(scoped.value());
+    callable(device, prefix, depth, out);
   }
-}
 
-void visit(udevxx::device const & device,
-           void (*callable)(udevxx::device const &, char const *, int, std::ostream &),
-           char const * prefix = "device",
-           std::ostream & out = std::cout,
-           int depth = 0)
-{
-  callable(device, prefix, depth, out);
-}
-
-std::ostream & indent(std::ostream & out, int depth)
-{
-  for (int i = 0; i < depth; ++i)
+  std::ostream & indent(std::ostream & out, int depth)
   {
-    out << '\t';
+    for (int i = 0; i < depth; ++i)
+    {
+      out << '\t';
+    }
+    return out;
   }
-  return out;
-}
 
-void print(udevxx::device const & device, char const * prefix, int depth, std::ostream & out = std::cout)
-{
-  indent(out, depth) << prefix << ": " << device.system_path() << '\n';
-  indent(out, depth + 1) << "subsystem: " << device.subsystem() << '\n';
-  indent(out, depth + 1) << "sysname: " << device.system_name() << '\n';
-  indent(out, depth + 1) << "sysnum: " << device.system_number() << '\n';
-  indent(out, depth + 1) << "devnum: " << device.device_number() << '\n';
-  indent(out, depth + 1) << "devpath: " << device.device_path() << '\n';
-  indent(out, depth + 1) << "devtype: " << device.device_type() << '\n';
-  indent(out, depth + 1) << "driver: " << device.driver() << '\n';
-  indent(out, depth + 1) << "initialized: " << (device.is_initialized() ? "yes" : "no") << '\n';
-  indent(out, depth + 1) << "action: " << device.action() << '\n';
-  indent(out, depth + 1) << "tags: \n";
-  map(device.tags(), [&](auto const & tag) { indent(out, depth + 2) << "- " << tag << '\n'; });
-  indent(out, depth + 1) << "properties: \n";
-  map(device.properties(),
-      [&](auto const & entry) { indent(out, depth + 2) << entry.first << ": " << entry.second << '\n'; });
-  indent(out, depth + 1) << "attributes: \n";
-  map(device.system_attributes(),
-      [&](auto const & attr) { indent(out, depth + 2) << attr << " => " << device[attr] << '\n'; });
-  map(device.parent(), [&](auto const & parent) { visit(parent, print, "parent", out, depth + 1); });
-}
+  void print(udevxx::device const & device, char const * prefix, int depth, std::ostream & out = std::cout)
+  {
+    indent(out, depth) << prefix << ": " << device.system_path() << '\n';
+    indent(out, depth + 1) << "subsystem: " << device.subsystem() << '\n';
+    indent(out, depth + 1) << "sysname: " << device.system_name() << '\n';
+    indent(out, depth + 1) << "sysnum: " << device.system_number() << '\n';
+    indent(out, depth + 1) << "devnum: " << device.device_number() << '\n';
+    indent(out, depth + 1) << "devpath: " << device.device_path() << '\n';
+    indent(out, depth + 1) << "devtype: " << device.device_type() << '\n';
+    indent(out, depth + 1) << "driver: " << device.driver() << '\n';
+    indent(out, depth + 1) << "initialized: " << (device.is_initialized() ? "yes" : "no") << '\n';
+    indent(out, depth + 1) << "action: " << device.action() << '\n';
+    indent(out, depth + 1) << "tags: \n";
+    map(device.tags(), [&](auto const & tag) { indent(out, depth + 2) << "- " << tag << '\n'; });
+    indent(out, depth + 1) << "properties: \n";
+    map(device.properties(),
+        [&](auto const & entry) { indent(out, depth + 2) << entry.first << ": " << entry.second << '\n'; });
+    indent(out, depth + 1) << "attributes: \n";
+    map(device.system_attributes(),
+        [&](auto const & attr) { indent(out, depth + 2) << attr << " => " << device[attr] << '\n'; });
+    map(device.parent(), [&](auto const & parent) { visit(parent, print, "parent", out, depth + 1); });
+  }
+
+}  // namespace traverse
 
 int main()
 {

ffe001a47a8156201f6bfffbacbd12a76dd31086
Felix Morgner <felix.morgner@hsr.ch>
2019-06-17 14:57:27 +0200

    devtree: switch to automatic memory management

--- src/devtree/devtree.cpp
+++ src/devtree/devtree.cpp
@@ -1,13 +1,20 @@
 #include <gtkmm.h>
 #include <iostream>
+#include <memory>
+
+template <typename WidgetType>
+auto get_widget(Glib::RefPtr<Gtk::Builder> builder, Glib::ustring const & name)
+{
+  WidgetType * widget = nullptr;
+  builder->get_widget(name, widget);
+  return std::shared_ptr<WidgetType>{widget};
+}
 
 int main(int argc, char ** argv)
 {
   auto app = Gtk::Application::create(argc, argv, "ch.hsr.ifs.devtree");
   auto builder = Gtk::Builder::create_from_resource("/main_window.glade");
+  auto main_window = get_widget<Gtk::ApplicationWindow>(builder, "main_window");
 
-  Gtk::Window * main_window;
-  builder->get_widget("window_main_window", main_window);
   app->run(*main_window);
-  delete main_window;
 }
